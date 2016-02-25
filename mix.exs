5e7202a5fad11838b3b35e8f3d49c25936cc5236
Louis Pilfold <louis@lpil.uk>
2016-02-25 18:24:53 +0000

    Support older Elixir versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PlugRedirect.Mixfile do
     [
       app: :plug_redirect,
       version: "0.1.0",
-      elixir: "~> 1.2",
+      elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps,
