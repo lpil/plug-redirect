70b94fc8116eeab5a19e3c0c92ffca1648b79513
Louis Pilfold <louis@lpil.uk>
2016-02-28 19:52:44 +0000

    DRY out tests with helper function

--- test/plug/redirect_test.exs
+++ test/plug/redirect_test.exs
@@ -11,6 +11,8 @@ defmodule Plug.RedirectTest do
     redirect 307, "/rock/on", "/roll/out"
 
     redirect "/no/status", "/301/default"
+
+    redirect "/blog/:slug", "/no-more-blog"
   end
 
   @opts MyPlug.init([])
@@ -26,36 +28,33 @@ defmodule Plug.RedirectTest do
 
   test "it can perform 301 redirects" do
     conn = :get |> conn("/foo/bar") |> MyPlug.call(@opts)
-    assert conn.state == :set
-    assert conn.status == 301
-    assert Plug.Conn.get_resp_header(conn, "location") == ["/a/redirect"]
+    assert_redirect(conn, 301, "/a/redirect")
   end
 
   test "it can perform 302 redirects" do
     conn = :get |> conn("/jump/up") |> MyPlug.call(@opts)
-    assert conn.state == :set
-    assert conn.status == 302
-    assert Plug.Conn.get_resp_header(conn, "location") == ["/get/down"]
+    assert_redirect(conn, 302, "/get/down")
   end
 
   test "it can perform 303 redirects" do
     conn = :get |> conn("/ra/wavy") |> MyPlug.call(@opts)
-    assert conn.state == :set
-    assert conn.status == 303
-    assert Plug.Conn.get_resp_header(conn, "location") == ["/by/droid"]
+    assert_redirect(conn, 303, "/by/droid")
   end
 
   test "it can perform 307 redirects" do
     conn = :get |> conn("/rock/on") |> MyPlug.call(@opts)
-    assert conn.state == :set
-    assert conn.status == 307
-    assert Plug.Conn.get_resp_header(conn, "location") == ["/roll/out"]
+    assert_redirect(conn, 307, "/roll/out")
   end
 
   test "when given no status it defaults to 301" do
     conn = :get |> conn("/no/status") |> MyPlug.call(@opts)
+    assert_redirect(conn, 301, "/301/default")
+  end
+
+
+  defp assert_redirect(conn, code, to) do
     assert conn.state == :set
-    assert conn.status == 301
-    assert Plug.Conn.get_resp_header(conn, "location") == ["/301/default"]
+    assert conn.status == code
+    assert Plug.Conn.get_resp_header(conn, "location") == [to]
   end
 end
